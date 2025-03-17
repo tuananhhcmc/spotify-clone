7a70a1e76dd161a00cc4c822c764bec5213180b6
arTdontlie <tuananh281003@gmail.com>
2025-03-17 11:48:11 +0700

    spotify api + login

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
     HomeIcon,
     HeartIcon,
@@ -8,13 +9,17 @@ import {
   } from '@heroicons/react/24/outline';
   
 import IconButton from './IconButton'
+import { useSession } from 'next-auth/react';
  
 const Divider = () => <hr className='border-t-[0.1px] border-gray-900' />
+
   
 const Sidebar = () => {
+    const {data:session} = useSession()
     return (
         <div className='text-gray-500 px-5 pt-5 pb-36 text-xs lg:text-sm border-r border-gray-900 h-screen overflow-y-scroll scrollbar-hidden sm:max-w-[12rem] lg:max-w-[15rem] hidden md:block'>
             <div className='space-y-4'>
+                {session?.user && <button>{session.user.name}-Log Out</button>}
                 <IconButton icon={HomeIcon} label='Home' />
                 <IconButton icon={MagnifyingGlassIcon} label='Search' />
                 <IconButton icon={BuildingLibraryIcon} label='Your Library' />
---
+++ src/pages/login.tsx
@@ -0,0 +1,47 @@
+import { GetServerSideProps } from "next"
+import {ClientSafeProvider, getProviders, signIn} from 'next-auth/react'
+import Image from 'next/image'
+
+
+
+
+interface Props {
+  providers : Awaited<ReturnType<typeof getProviders>>
+}
+
+const Login = ({providers}: Props) => {
+  const {name: provideName, id :providerId} = providers?.spotify as ClientSafeProvider
+
+  return(
+    <div className='flex flex-col justify-center items-center bg-black h-screen'>
+    <div className='mb-6'>
+      <Image
+        src="/spotify_logo.png" 
+        alt='Spotify Logo'
+        height={200}
+        width={200}
+      />
+    </div>
+
+    <button
+      className='bg-[#18D860] text-white p-5 rounded-full'
+      onClick={() => {
+        signIn(providerId, { callbackUrl: '/' })
+      }}
+    >
+      Login with {provideName}
+    </button>
+  </div>
+  )
+}
+
+export default Login;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+const providers = await getProviders(); //
+
+return {
+  props: {
+    providers, 
+  },
+};
